6e597c111e138586262307cb7e75e6c5e8f27ab5
Pravin Neema <pneema@yahoo-inc.com>
2014-06-03 19:35:43 -0700

    Initial Commit

---
+++ tipdemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipdemo
+//
+//  Created by Pravin Neema on 6/3/14.
+//  Copyright (c) 2014 Pravin Neema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
