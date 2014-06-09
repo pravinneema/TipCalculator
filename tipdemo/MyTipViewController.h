09f61c034eba3ea6fc3f89a0bfe0e9a9bd25ace0
Pravin Neema <pneema@yahoo-inc.com>
2014-06-08 22:11:43 -0700

    First commit

---
+++ tipdemo/MyTipViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTipViewController.h
+//  tipdemo
+//
+//  Created by Pravin Neema on 6/4/14.
+//  Copyright (c) 2014 Pravin Neema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTipViewController : UIViewController
+
+@end
